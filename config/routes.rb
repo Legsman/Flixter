6d3180c2d5411de4bd737bd1c666c4d33fe4c6d8
Antoine <maciaantoine@yahoo.fr>
2016-07-03 11:03:46 +0000

    add drag and drop for sections with javascript

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Flixter::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
-    resources :enrollments, only: :create
+    resources :enrollments, only: [:create]
   end
   resources :lessons, only: [:show]
   namespace :instructor do 
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
